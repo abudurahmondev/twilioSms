d1bb17040a7f5bb4e0ab9e25689ddbbbd8395a3a
abudurahmondev <k809292@gmail.com>
2024-06-11 10:51:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const twilio = require('twilio')
+const dotenv = require('dotenv')
+const app = express()
+
+dotenv.config()
+
+async function sendSms(){
+    const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN)
+
+    try {
+        const message = await client.messages.create({
+            body: 'This is sms',
+            from: '+15624576601',
+            to: process.env.PHONE_NUMBER
+        })
+        console.log(message.sid, "Message sent");
+    } catch (error) {
+        console.error(error, 'Message not sent')
+    }
+}
+
+sendSms()
+
+app.listen(5000, () => console.log(`Server is on`))
